74a85f7cc0e5479bf4988f60d0ddb70284c87e4d
Steve <blackfyst@gmail.com>
2023-11-05 15:48:23 -0500

    Allow printing more than one error message

--- all_checks.py
+++ all_checks.py
@@ -1,30 +1,34 @@
 #!/usr/bin/env python3
 
 import os
+import shutil
 import sys
 
 def check_reboot():
     """Returns True if the computer has a pending reboot."""
     return os.path.exists("/run/reboot-required")
 
-def check_full_disk(disk, min_gb, min_percent):
-"""Returns True if there isn't enough disk space, False otherwise."""
+def check_disk_full(disk, min_gb, min_percent):
+    """Returns True if there isn't enough disk space, False otherwise."""
     du = shutil.disk_usage(disk)
     # Calculate the percentage of free space
-    percent_free = 100 * du-free / du.total
+    percent_free = 100 * du.free / du.total
     # Calculate how many free gigabytes
     gigabytes_free = du.free / 2**30
     if percent_free < min_percent or gigabytes_free < min_gb:
-      return True
+        return True
     return False
 
+def check_root_full():
+    """Returns True if the root partition is full, False otherwise."""
+    return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
 def main():
-    if check_reboot():
-        print("Pending Reboot.")
-        sys.exit(1)
-    if check_disk_full(disk="/",min_gb=2,min_percent=10)
-        print("Disk_full.")
-        sys.exit(1)
+    checks=[(check_reboot, "Pending reboot."), (check_root_full, "Root partition full.")]
+    for check, msg in checks:
+        if check():
+            print(msg)
+            sys.exit(1)
     print("Everything ok.")
     sys.exit(0)
 
